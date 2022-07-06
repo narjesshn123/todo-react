17e30fcfd0b5dd44a4f7cd0dc27cb16a565b3f9c
unknown <narjessheikhali@gmail.com>
2022-07-06 16:40:17 +0430

    add handle complete

--- todo/src/component/AllTodo/AllTodo.jsx
+++ todo/src/component/AllTodo/AllTodo.jsx
@@ -1,10 +1,10 @@
 export const AllTodo = [
-    {id:1,tittle:"wash dishes",description:"please be wash very clearly",
+    {id:1, tittle:"wash dishes", description:"please be wash very clearly", complete:false,
 },
-    {id:2, tittle:"wash dishes", description:"please be wash very clearly",
+    {id:2, tittle:"wash dishes", description:"please be wash very clearly", complete:false,
 },
-    {id:3, tittle:"wash dishes", description:"please be wash very clearly",
+    {id:3, tittle:"wash dishes", description:"please be wash very clearly", complete:false,
 },
-    {id:4, tittle:"wash dishes", description:"please be wash very clearly",
+    {id:4, tittle:"wash dishes", description:"please be wash very clearly", complete:false,
 },
 ]
--- todo/src/component/Todo/Todo.jsx
+++ todo/src/component/Todo/Todo.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 
 const TodoList = ({item, setItem, ite}) => {
-const handleDelete = (id) => {
+const handleDelete = () => {
 setItem(item.filter(oldId=>oldId.id!==ite.id))
-console.log(ite)
+}
+const handleComplete = () =>{
+  setItem(item.map(todo=>{
+    if(todo.id === ite.id){
+      return{...todo, complete: !todo.complete}
+    }
+    else{
+      return todo
+    }
+  }))
 }
   return (
     <div className='todo'>
-      <li className='todo-item'>{ite.tittle}</li>
-        <button className='complete-btn'>
+      <li className={`todo-item ${ite.complete ? "completed":""}`}>{ite.tittle}</li>
+        <button onClick={handleComplete} className='complete-btn'>
           <i className='fas fa-check'></i>
         </button>
         <button onClick={()=>handleDelete(ite.id)} className='trash-btn'>
